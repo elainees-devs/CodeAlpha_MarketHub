f151cafc459b46a0f1a2cd84d5c1b657da0ccec4
elainees-devs <elainees.devs@gmail.com>
2026-04-16 23:14:22 +0300

    chore(middleware): add centralized middleware exports

---
+++ server/src/middlewares/index.ts
@@ -0,0 +1,4 @@
+export {authenticateMiddleware} from './auth.middleware';
+export {errorHandler} from './error.middleware';
+export {requireRole} from './role.middleware';
+export {validate} from './validate.middleware'
