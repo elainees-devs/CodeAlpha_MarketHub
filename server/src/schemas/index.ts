5d01632356e4c235de04e5e38974eb1cf862db12
elainees-devs <elainees.devs@gmail.com>
2026-04-16 14:10:38 +0300

    chore(schemas): centralize and export all validation schemas

---
+++ server/src/schemas/index.ts
@@ -0,0 +1,96 @@
+// Login Schemas
+export { LoginSchema } from "./auth.schema";
+
+// User Schemas
+export {
+  UserSchema,
+  CreateUserSchema,
+  UpdateUserSchema,
+  DeleteUserSchema,
+  UserResponseSchema,
+} from "./user.schema";
+
+// Permission Schemas
+export {
+  PermissionEnumSchema,
+  PermissionSchema,
+  CreatePermissionSchema,
+  UpdatePermissionSchema,
+  DeletePermissionSchema,
+  PermissionResponseSchema,
+
+  RolePermissionSchema,
+  AssignRolePermissionSchema,
+  RemoveRolePermissionSchema,
+  RolePermissionResponseSchema,
+} from "./permission.schema";
+
+// Role Schemas
+export {
+  RoleEnumSchema,
+  RoleSchema,
+  CreateRoleSchema,
+  UpdateRoleSchema,
+  DeleteRoleSchema,
+  RoleResponseSchema,
+  UserRoleSchema,
+  AssignUserRoleSchema,
+  RemoveUserRoleSchema,
+  UserRoleResponseSchema,
+} from "./role.schema";
+
+// Product Schemas
+export {
+  ProductSchema,
+  CreateProductSchema,
+  UpdateProductSchema,
+  DeleteProductSchema,
+  ProductResponseSchema,
+
+  ProductImageSchema,
+  CreateProductImageSchema,
+  UpdateProductImageSchema,
+  DeleteProductImageSchema,
+  ProductImageResponseSchema,
+} from "./product.schema";
+
+// Order Schemas
+export {
+  OrderSchema,
+  CreateOrderSchema,
+  UpdateOrderSchema,
+  DeleteOrderSchema,
+  OrderResponseSchema,
+
+  OrderItemSchema,
+  CreateOrderItemSchema,
+  UpdateOrderItemSchema,
+  DeleteOrderItemSchema,
+  OrderItemResponseSchema,
+} from "./order.schema";
+
+// Shipment Schemas
+export {
+  ShipmentStatusEnumSchema,
+  ShipmentSchema,
+  CreateShipmentSchema,
+  UpdateShipmentSchema,
+  DeleteShipmentSchema,
+  ShipmentResponseSchema,
+} from "./shipment.schema";
+
+// Cart Schemas
+export {
+  CartSchema,
+  CreateCartSchema,
+  UpdateCartSchema,
+  DeleteCartSchema,
+  CartResponseSchema,
+  
+  CartItemSchema,
+  CreateCartItemSchema,
+  UpdateCartItemSchema,
+  RemoveCartItemSchema,
+  DeleteCartItemSchema,
+  CartItemResponseSchema,
+} from "./cart.schema";
