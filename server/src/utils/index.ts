471179270cf57e1834ffce2eccebd8a962f1945d
elainees-devs <elainees.devs@gmail.com>
2026-04-16 14:55:15 +0300

    feat(utils): add unified exports for auth, prisma, logger, and helpers

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,1 +1,9 @@
+import e from 'express';
+
 export { ROLES, PERMISSIONS, ORDER_STATUS, PAYMENT_STATUS, PAYMENT_PROVIDERS, SHIPMENT_STATUS, ROLE_PERMISSIONS } from './constants'
+export { ApiError } from './app-error';
+export { generateToken, verifyToken } from './token';
+export { prisma } from './prisma';
+export { default as logger } from './winstolLogger';
+export { phoneRegex } from './phoneRegex';
+export { generateUUID } from './generateId';
