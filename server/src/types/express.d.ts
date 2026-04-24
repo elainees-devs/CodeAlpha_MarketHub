81677be145e6d35272d4c7b11101424dd011e0aa
elainees-devs <elainees.devs@gmail.com>
2026-04-24 19:02:29 +0300

    chore(types): extend Express request with session support and refine auth user typing

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -3,7 +3,8 @@ import { IAuthUser } from "./interfaces.types";
 declare global {
   namespace Express {
     interface Request {
-      user?: IAuthUser | null;
+      user?: IAuthUser;
+      session_id?: string;
     }
   }
 }
