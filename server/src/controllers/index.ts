69441b1cc9556d085aab812d22c8b10432985380
elainees-devs <elainees.devs@gmail.com>
2026-04-24 18:53:09 +0300

    chore(controllers): organize and centralize controller exports by domain

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,31 @@
+// RBAC Controllers
+export {authController} from "./auth.controller";
+export {roleController} from "./role.controller";
+export {permissionController} from "./permission.controller";
+export {userRoleController} from "./userRole.controller";
+export {userPermissionController} from "./userPermission.controller";
+
+// Audit Log Controllers
+export {auditLogController} from "./auditLog.controller";
+
+// User Controllers
+export {userController} from "./user.controller";
+
+// Product related Controllers
+export {categoryController} from "./category.controller";
+export {subcategoryController} from "./subCategory.controller";
+export {discountController} from "./discount.controller";
+export {productController} from "./product.controller";
+
+
+
+// cart related controllers
+export {cartController} from "./cart.controller";
+export {cartItemController} from "./cartItem.controller";
+
+// order, shipment and payment controllers
+export {orderController} from "./order.controller";
+export {orderItemController} from "./orderItem.controller";
+export {shipmentController} from "./shipment.controller";
+export {paymentController} from "./payment.controller";
+
