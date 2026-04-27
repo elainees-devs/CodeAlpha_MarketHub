3e6aa09770b0ebff90ebdc739c31aed415bd44df
elainees-devs <elainees.devs@gmail.com>
2026-04-27 19:49:23 +0300

    fix(routes): clean up AppRoutes and add products page

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -2,22 +2,25 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
+import ProductsPage from "../pages/Products";
 import Register from "../pages/auth/Register";
 import Login from "../pages/auth/Login";
 import Cart from "../pages/Cart";
 import Checkout from "../pages/Checkout";
-
 import ProtectedRoute from "./ProtectedRoute";
+
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      a{/* Public routes */}
+      {/* Public routes */}
       <Route path="/" element={<Home />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/checkout" element={<Checkout />} />
+      <Route path="/products" element={<ProductsPage />} />
+
       {/* Protected checkout */}
       <Route
         path="/checkout"
