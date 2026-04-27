b041e9845dd0e048c92f858857725a0e7546b95d
elainees-devs <elainees.devs@gmail.com>
2026-04-27 15:15:59 +0300

    feat(product): add basic ProductDetails page scaffold

---
+++ client/src/pages/ProductDetails.tsx
@@ -0,0 +1,10 @@
+const ProductDetails:React.FC = () => {
+  return (
+    <div>
+      <h1>Product Details</h1>
+      {/* Product details content goes here */}
+    </div>
+  );
+};
+
+export default ProductDetails;
