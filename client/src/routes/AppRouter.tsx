43683a9ee2170c17b3ac0c364c3211ddf14ae345
elainees-devs <elainees.devs@gmail.com>
2026-04-27 12:24:55 +0300

    feat(routes): add cart, login, and protected checkout routes
    - Added Cart and Login public routes
    - Integrated Checkout route with ProtectedRoute guard
    - Expanded routing structure beyond Home page
    - Prepared app for authenticated user flows

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Home from "../pages/Home";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import Login from "../pages/auth/Login";
+
+import ProtectedRoute from "./ProtectedRoute";
+
+
+
 
 const AppRoutes: React.FC = () => {
   return (
-    <Routes>
+    <Routes>a
+      {/* Public routes */}
       <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* Protected checkout */}
+      <Route
+        path="/checkout"
+        element={
+          <ProtectedRoute>
+            <Checkout />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
