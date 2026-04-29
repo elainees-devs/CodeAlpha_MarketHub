36261d7769775b14b1ff0b70b70b15cf079bcb6a
elainees-devs <elainees.devs@gmail.com>
2026-04-29 22:14:58 +0300

    feat(order): secure order-success page using ProtectedRoute

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -7,26 +7,35 @@ import Register from "../pages/auth/Register";
 import Login from "../pages/auth/Login";
 import Cart from "../pages/Cart";
 import Checkout from "../pages/Checkout";
+import OrderSuccess from "../pages/OrderSuccess";
 import ProtectedRoute from "./ProtectedRoute";
-
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      {/* Public routes */}
+      {/* PUBLIC ROUTES */}
       <Route path="/" element={<Home />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/checkout" element={<Checkout />} />
       <Route path="/products" element={<ProductsPage />} />
 
-      {/* Protected checkout */}
+      {/* PROTECTED ROUTES */}
       <Route
         path="/checkout"
         element={
           <ProtectedRoute>
             <Checkout />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ORDER SUCCESS (FIXED) */}
+      <Route
+        path="/order-success/:id"
+        element={
+          <ProtectedRoute>
+            <OrderSuccess />
           </ProtectedRoute>
         }
       />
