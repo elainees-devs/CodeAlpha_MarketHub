426cb64482e140e7a0c4bedfc7c790d2a5a049b1
elainees-devs <elainees.devs@gmail.com>
2026-04-30 10:02:41 +0300

    chore: remove test config and json files

--- server/jest.config.ts
+++
@@ -1,9 +0,0 @@
-// jest.config.ts
-export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/tests"],
-  testMatch: ["**/*.test.ts"],
-  setupFilesAfterEnv: ["<rootDir>/tests/setup/jest.setup.ts"],
-  clearMocks: true,
-};
